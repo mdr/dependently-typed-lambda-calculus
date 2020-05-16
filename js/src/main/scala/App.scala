106608bcd9e404e9185050a57ca2d140c18a7d44
Matt Russell <MattRussellUK@gmail.com>
2020-05-16 21:20:44 +0100

    wip

--- js/src/main/scala/App.scala
+++ js/src/main/scala/App.scala
@@ -14,30 +14,21 @@ object App {
     private def onDependentPressed: Callback = $.modState(_.copy(tab = 0))
 
     def render(state: State) = {
-      <.div(
-        <.nav(^.className := "navbar navbar-expand-lg navbar-dark bg-dark")(
-          <.div(^.className := "container-fluid")(
-            <.a(^.className := "navbar-brand", ^.href := "#")("λ-Calculus"),
-            <.div(^.className := "collapse navbar-collapse", ^.id := "navbarsExample07")(
-              <.ul(^.className := "navbar-nav mr-auto")(
-                <.li(^.className := (if (state.tab == 0) "nav-item active" else "nav-item"))(
-                  <.a(^.className := "nav-link", ^.href := "#", ^.onClick --> onDependentPressed)(
-                    "Dependently-typed"
-                  )
-                ),
-                <.li(^.className := (if (state.tab == 1) "nav-item active" else "nav-item"))(
-                  <.a(^.className := "nav-link", ^.href := "#", ^.onClick --> onSimplePressed)(
-                    "Simply-typed")
-                ),
-              ),
-            )
-          )
+      <.div(^.`class` := "container",
+        <.div(^.className := "d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow",
+          <.h5(^.className := "my-0 mr-md-auto font-weight-normal", "λ-Calculus"),
+          <.nav(^.className := "my-2 my-md-0 mr-md-3",
+            <.a(^.className := s"p-2 text-dark${if (state.tab == 0) " font-weight-bold" else ""}", ^.href := "#", ^.onClick --> onDependentPressed)(
+              "Dependently-typed"
+            ),
+            <.a(^.className := s"p-2 text-dark${if (state.tab == 1) " font-weight-bold" else ""}", ^.href := "#", ^.onClick --> onSimplePressed)(
+              "Simply-typed")
+          ),
         ),
-        <.div(^.`class` := "container-fluid",
-          <.p(),
-          <.p("A Scala port of ", <.a(^.href := "https://www.andres-loeh.de/LambdaPi/", <.em("A Tutorial Implementation of a Dependently Typed Lambda Calculus")), " by Andres Löh, Conor McBride and Wouter Swierstra. Code can be found on ", <.a(^.href := "https://github.com/mdr/dependently-typed-lambda-calculus", "Github"), "."),
-          DependentlyTypedTab.dependentlyTypedTab().when(state.tab == 0),
-          SimplyTypedTab.simplyTypedTab().when(state.tab == 1)))
+        <.p(),
+        <.p("A Scala port of ", <.a(^.href := "https://www.andres-loeh.de/LambdaPi/", <.em("A Tutorial Implementation of a Dependently Typed Lambda Calculus")), " by Andres Löh, Conor McBride and Wouter Swierstra. Code can be found on ", <.a(^.href := "https://github.com/mdr/dependently-typed-lambda-calculus", "Github"), "."),
+        DependentlyTypedTab.dependentlyTypedTab().when(state.tab == 0),
+        SimplyTypedTab.simplyTypedTab().when(state.tab == 1))
     }
 
   }
