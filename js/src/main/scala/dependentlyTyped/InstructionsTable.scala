c6d87a911cee9371aabeb9c025fed2626e3bed39
Matt Russell <MattRussellUK@gmail.com>
2020-05-18 07:59:46 +0100

    wip

--- js/src/main/scala/dependentlyTyped/InstructionsTable.scala
+++ js/src/main/scala/dependentlyTyped/InstructionsTable.scala
@@ -21,7 +21,7 @@ object InstructionsTable {
             <.td(<.code("""multiply 2 3""")),
           ),
           <.tr(
-            <.td("Prove that for any natural numbers n and m, n×m=m×n"),
+            <.td("Prove that for any natural numbers n and m, n + m = m + n"),
             <.td(<.code("""let plus_comm = natElim (\n -> forall (m :: Nat) . Eq Nat (plus n m) (plus m n)) (\m -> symm Nat (plus m 0) m (pNPlus0isN m)) (\n p m -> tran Nat (Succ (plus n m)) (Succ (plus m n)) (plus m (Succ n)) (leibniz Nat Nat Succ (plus n m) (plus m n) (p m)) ((natElim (\n -> forall (m :: Nat) . Eq Nat (Succ (plus n m)) (plus n (Succ m))) (\m -> Refl Nat (Succ m)) (\n p m-> leibniz Nat Nat Succ (Succ (plus n m)) (plus n (Succ m)) (p m))) m n)) :: forall (n :: Nat) (m :: Nat) . Eq Nat (plus n m) (plus m n)""")),
           )
         ),
